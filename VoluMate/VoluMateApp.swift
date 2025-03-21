268b99f6a476158939beb7f786c009cc9e1e9833
developerkamol <developerkamol@gmail.com>
2025-03-22 03:44:26 +0500

    Initial Commit

---
+++ VoluMate/VoluMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoluMateApp.swift
+//  VoluMate
+//
+//  Created by Kamol Madaminov on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct VoluMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
